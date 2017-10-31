8698c0d4e92e6419a23c284109473d5e799eee25
Jonathan Peel <me@jonathanpeel.co.za>
2017-10-31 10:26:03 +0200

    Add basics for CharString

---
+++ src/Simples/Strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Simples
+{
+    public class CharString : IComparable<CharString>, IComparable<string>
+    {
+        readonly string inner;
+
+        public override string ToString() => inner;
+        public int CompareTo(CharString other) => CompareTo(other.inner);
+        public int CompareTo(string other) => string.Compare(inner, other, StringComparison.Ordinal);
+
+        CharString(string from) => inner = from.ToUpper();
+        CharString(char[] from) => inner = new CharString(new string(from));
+
+        public static implicit operator CharString(string from) => new CharString(from);
+        public static implicit operator CharString(char[] from) => new CharString(from);
+        public static implicit operator string(CharString from) => from.ToString();
+
+        public static CharString operator ++(CharString @this)
+        {
+            if (string.IsNullOrEmpty(@this)) return "A";
+            var chars = @this.inner.ToCharArray();
+            var i = chars.Length - 1;
+            if (++chars[i] <= 'Z') return chars;
+            CharString fore = chars.Take(i).ToArray();
+            return ++fore + "A";
+        }
+    }
+}
---
+++ tests/SimplesTests/StringsTests.cs
@@ -0,0 +1,42 @@
+﻿using Simples;
+using Xunit;
+
+namespace SimplesTests
+{
+    public class CharStringTests
+    {
+        [Theory]
+        [InlineData("A", "B")]
+        [InlineData("C", "D")]
+        [InlineData("E", "F")]
+        [InlineData("G", "H")]
+        [InlineData("I", "J")]
+        [InlineData("K", "L")]
+        [InlineData("M", "N")]
+        [InlineData("O", "P")]
+        [InlineData("Q", "R")]
+        public void Inc_Single(string start, string expected) => Check(start, expected);
+
+        [Theory]
+        [InlineData("AA", "AB")]
+        [InlineData("GG", "GH")]
+        public void Inc_Multi(string start, string expected) => Check(start, expected);
+
+        [Theory]
+        [InlineData("AZ", "BA")]
+        [InlineData("GZ", "HA")]
+        public void Inc_Roll(string start, string expected) => Check(start, expected);
+
+        [Theory]
+        [InlineData("AZZ", "BAA")]
+        [InlineData("GZZ", "HAA")]
+        public void Inc_MultiRoll(string start, string expected) => Check(start, expected);
+
+        [Theory]
+        [InlineData("ZZ", "AAA")]
+        [InlineData("ZZZ", "AAAA")]
+        public void Inc_FullRoll(string start, string expected) => Check(start, expected);
+
+        void Check(CharString start, CharString expected) => Assert.Equal(expected, ++start);
+    }
+}
